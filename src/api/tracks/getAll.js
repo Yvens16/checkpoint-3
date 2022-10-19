54e122919d6748c06fae932687d9f5c68dbea68b
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-10-19 12:24:48 +0200

    getAll and getOne ok

--- src/api/tracks/getAll.js
+++ src/api/tracks/getAll.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-  // your code here !
+const database = require('../../db');
+
+module.exports = (_, res) => {
+  let sql = 'select * from track';
+
+  database
+    .query(sql)
+    .then(([tracks]) => {
+      res.json(tracks);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    });
 };
