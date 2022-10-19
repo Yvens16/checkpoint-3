1a2d729b5b8fd55b762b2f6c67a6ffc03bb432c5
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-10-19 10:14:30 +0200

    Start project and create database.sql

---
+++ database.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS track;
+DROP TABLE IF EXISTS album;
+
+CREATE TABLE album (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  picture VARCHAR(255),
+  artist VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE track (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(128),
+  youtube_url VARCHAR(255),
+  id_album INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_album) REFERENCES album(id)
+);
