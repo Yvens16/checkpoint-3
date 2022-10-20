b3ba8c4d6787363d145f4cb32bccfdd86a47d578
dexter-cut <r.bassot.wcs@gmail.com>
2022-10-20 11:20:26 +0200

    test 11/11 ok npm run fix! checkpoint done

--- src/api/tracks/delete.js
+++ src/api/tracks/delete.js
@@ -3,7 +3,7 @@ const { mydb } = require('../../Cp3');
 module.exports = (req, res) => {
   const id = parseInt(req.params.id, 10);
   mydb
-    .query("DELETE from track where id=?", [id])
+    .query('DELETE from track where id=?', [id])
     .then(() => res.sendStatus(204))
     .catch((err) => {
       res.status(500).json('error in delete action', err);
